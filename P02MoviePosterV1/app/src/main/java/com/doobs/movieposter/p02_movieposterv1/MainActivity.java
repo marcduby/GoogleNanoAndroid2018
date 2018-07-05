8d9774772c5faa9ac49873c50bb44e3f9132aaf9
mduby <mduby@broadinstitute.org>
2018-07-04 23:03:18 -0400

    P02-added first build of project

---
+++ P02MoviePosterV1/app/src/main/java/com/doobs/movieposter/p02_movieposterv1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.doobs.movieposter.p02_movieposterv1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
