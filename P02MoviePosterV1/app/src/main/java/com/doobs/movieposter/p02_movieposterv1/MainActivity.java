44b8fb577f14365c84277fc7a6f00978eca79d06
mduby <mduby@broadinstitute.org>
2018-07-09 13:48:04 -0400

    P-02: added manin activity menu for sorting

--- P02MoviePosterV1/app/src/main/java/com/doobs/movieposter/p02_movieposterv1/MainActivity.java
+++ P02MoviePosterV1/app/src/main/java/com/doobs/movieposter/p02_movieposterv1/MainActivity.java
@@ -1,9 +1,12 @@
 package com.doobs.movieposter.p02_movieposterv1;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
@@ -36,6 +39,43 @@ public class MainActivity extends AppCompatActivity {
                 launchMovieDetailActivity(position);
             }
         });
+    }
+
+    @Override
+    /**
+     * inflate the sort menu
+     */
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // inflate the main activity menu
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        // return true for success
+        return true;
+    }
+
+    @Override
+    /**
+     * handle the menu selection
+     */
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // get the id of the menu item clicked
+        int itemThatWasClickedId = item.getItemId();
+
+        // change the ordering if need be
+        if (itemThatWasClickedId == R.id.action_order_by_user_rating) {
+            Context context = MainActivity.this;
+            String textToShow = "List sorted by user rating";
+            Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+            return true;
+
+        } else if (itemThatWasClickedId == R.id.action_order_by_most_popular) {
+            Context context = MainActivity.this;
+            String textToShow = "List sorted by most popular";
+            Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     /**
