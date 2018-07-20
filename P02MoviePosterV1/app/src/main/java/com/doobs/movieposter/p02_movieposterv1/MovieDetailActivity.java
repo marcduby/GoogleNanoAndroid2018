b0ba5fc06804e0d31dadad91a671f947ec4d8b8e
mduby <mduby@broadinstitute.org>
2018-07-20 14:14:38 -0400

    P-02: working simple movie detail linear layout

--- P02MoviePosterV1/app/src/main/java/com/doobs/movieposter/p02_movieposterv1/MovieDetailActivity.java
+++ P02MoviePosterV1/app/src/main/java/com/doobs/movieposter/p02_movieposterv1/MovieDetailActivity.java
@@ -27,6 +27,7 @@ public class MovieDetailActivity extends AppCompatActivity {
     private TextView releaseDateTextView;
     private ImageView movieImageView;
 
+    // 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // local variables
@@ -73,6 +74,11 @@ public class MovieDetailActivity extends AppCompatActivity {
      * @param movieBean                  the movie to display
      */
     private void populateUI(MovieBean movieBean) {
+        // instance vartiables
+        TextView dataView = null;
+        TextView labelView = null;
+        String dataString = null;
+
         // populate the image
         this.movieImageView = findViewById(R.id.movie_detail_poster_iv);
         String imageUrl = MovieUtils.getImageUrlString(movieBean.getImageUrl(), false);
@@ -83,20 +89,49 @@ public class MovieDetailActivity extends AppCompatActivity {
         // display the thumbnail poster image
         Picasso.get()
                 .load(imageUrl)
-//                .fit()
                 .into(movieImageView);
 
 
         // add the name
-        nameTextView = findViewById(R.id.movie_detail_title_tv);
-        if (movieBean.getName().isEmpty()) {
-//            TextView labelView = findViewById(R.id.name_label_tv);
-            nameTextView.setVisibility(View.GONE);
-//            labelView.setVisibility(View.GONE);
+        dataView = findViewById(R.id.movie_detail_title_tv);
+        labelView = findViewById(R.id.name_label_tv);
+        dataString = movieBean.getName();
+        this.bindDataToViews(labelView, dataView, dataString);
+
+        // add the plot synopsis
+        dataView = findViewById(R.id.movie_detail_synopsis_tv);
+        labelView = findViewById(R.id.synopsis_label_tv);
+        dataString = movieBean.getPlotSynopsis();
+        this.bindDataToViews(labelView, dataView, dataString);
+
+        // add the user rating
+        dataView = findViewById(R.id.movie_detail_user_rating_tv);
+        labelView = findViewById(R.id.user_rating_label_tv);
+        dataString = (movieBean.getRating() == null ? null : movieBean.getRating().toString());
+        this.bindDataToViews(labelView, dataView, dataString);
+
+        // add the release data
+        dataView = findViewById(R.id.movie_detail_release_date_tv);
+        labelView = findViewById(R.id.release_date_label_tv);
+        dataString = movieBean.getReleaseDate();
+        this.bindDataToViews(labelView, dataView, dataString);
+
+    }
+
+    /**
+     * apply data to the text views; hide them if there is no data
+     *
+     * @param labelView
+     * @param dataView
+     * @param dataString
+     */
+    private void bindDataToViews(TextView labelView, TextView dataView, String dataString) {
+        if (dataString.isEmpty()) {
+            dataView.setVisibility(View.GONE);
+            labelView.setVisibility(View.GONE);
 
         } else {
-            nameTextView.setText(movieBean.getName());
+            dataView.setText(dataString);
         }
-
     }
 }
