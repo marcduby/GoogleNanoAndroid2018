b741284053235ad9038489971a2f12956ac12f98
mduby <mduby@broadinstitute.org>
2018-09-08 18:26:48 -0400

    P-04: added media player; not showing up

--- P04_BakingAppV1/app/src/main/java/com/doobs/baking/RecipeStepDetailFragment.java
+++ P04_BakingAppV1/app/src/main/java/com/doobs/baking/RecipeStepDetailFragment.java
@@ -1,6 +1,9 @@
 package com.doobs.baking;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,11 +14,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.doobs.baking.adapter.RecipeStepRecyclerAdapter;
 import com.doobs.baking.bean.RecipeBean;
 import com.doobs.baking.bean.RecipeStepBean;
 import com.doobs.baking.util.BakingAppConstants;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import java.util.ArrayList;
 
@@ -29,6 +45,8 @@ public class RecipeStepDetailFragment extends Fragment {
     // instance variables
     private final String TAG = this.getClass().getName();
     private RecipeStepBean recipeStepBean;
+    private SimpleExoPlayer simpleExoPlayer;
+    private SimpleExoPlayerView simpleExoPlayerView;
 
     /**
      * default constructor
@@ -58,8 +76,75 @@ public class RecipeStepDetailFragment extends Fragment {
         TextView descriptionTextView = rootView.findViewById(R.id.recipe_step_detail_description_fragment_tv);
         descriptionTextView.setText(recipeStepBean.getDescription());
 
+        // create the exo player
+        this.simpleExoPlayerView = (SimpleExoPlayerView)rootView.findViewById(R.id.exoplayer_view);
+
+        // set the default art work
+        this.simpleExoPlayerView.setDefaultArtwork(BitmapFactory.decodeResource(this.getResources(), R.drawable.question_mark));
+
+        // create the uri from the recipe url string
+        if (recipeStepBean.getVideoUrl() != null) {
+            if (recipeStepBean.getVideoUrl().length() > 0) {
+                Uri reciperUri = Uri.parse(recipeStepBean.getVideoUrl());
+
+                // set the uri on the media player
+                // initialize the player
+                this.initializePlayer(rootView.getContext(), reciperUri);
+
+                Toast.makeText(rootView.getContext(), "started video: " + recipeStepBean.getVideoUrl(), Toast.LENGTH_LONG).show();
+
+            } else {
+                Toast.makeText(rootView.getContext(), "no video for video: " + recipeStepBean.getVideoUrl(), Toast.LENGTH_LONG).show();
+            }
+        }
+
         // return the view
         return rootView;
+    }
+
+    /**
+     * initialize the media player
+     *
+     * @param context
+     * @param mediaUri
+     */
+    private void initializePlayer(Context context, Uri mediaUri) {
+        if (this.simpleExoPlayer == null) {
+            // create the exoplayer
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            this.simpleExoPlayer = ExoPlayerFactory.newSimpleInstance(context, trackSelector, loadControl);
+            this.simpleExoPlayerView.setPlayer(this.simpleExoPlayer);
+
+            // prepare the media source
+            String userAgent = Util.getUserAgent(context, "ClassicalMusicQuiz");
+            MediaSource mediaSource = new ExtractorMediaSource(mediaUri, new DefaultDataSourceFactory(context, userAgent),
+                    new DefaultExtractorsFactory(), null, null);
+
+            this.simpleExoPlayer.prepare(mediaSource);
+            this.simpleExoPlayer.setPlayWhenReady(true);
+        }
+    }
+
+    /**
+     * releases the media player
+     *
+     */
+    private void releasePlayer() {
+        // NOTE - also call this in onPause and onStop when the app is not visible
+        if (this.simpleExoPlayer != null) {
+            this.simpleExoPlayer.stop();
+            this.simpleExoPlayer.release();
+            this.simpleExoPlayer = null;
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        // release the media player
+        this.releasePlayer();;
     }
 
     /**
